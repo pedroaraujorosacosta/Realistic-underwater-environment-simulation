be8d9f3515bf584bf493c7028b0d627061733bc4
pedroaraujorosacosta <pedro.araujo.rosa.costa@gmail.com>
2015-04-20 18:04:33 +0100

    Added a rectangle where we will show our RTT on the screen.

--- ASiameseDream/Application.cpp
+++ ASiameseDream/Application.cpp
@@ -13,18 +13,21 @@
 using namespace GeneratorNodes;
 
 //-------------------------------------------------------------------------------------
-Application::Application(void)
+Application::Application(void) : diffuseRed(0.0f), diffuseGreen(0.0f), diffuseBlue(0.0f), specularRed(0.0f),
+	specularGreen(0.0f), specularBlue(0.0f), INIT_ANGLE(Ogre::Math::PI / 6), plantChanged(false)
 {
 	sub = 0;
+	angle = INIT_ANGLE;
 }
 //-------------------------------------------------------------------------------------
 Application::~Application(void)
 {
+	delete plantQuad;
 }
 //-------------------------------------------------------------------------------------
 void Application::destroyScene(void)
 {
-
+	//mSceneMgr->destroySceneNode(plants[FRACTAL_PLANT].plantNode);
 }
 //-------------------------------------------------------------------------------------
 void getTerrainImage(bool flipX, bool flipY, Ogre::Image& img)
@@ -128,19 +131,8 @@ void Application::createScene(void)
 	if (mRoot->getRenderSystem()->getCapabilities()->hasCapability(Ogre::RSC_INFINITE_FAR_PLANE))
 		mCamera->setFarClipDistance(0);   // enable infinite far clip distance if we can
 
-	// create the plants
-	plants[FRACTAL_PLANT].numGenerations = 3;
-	plants[FRACTAL_PLANT].plantLocation = Ogre::Vector3(1683, 49.7f, 2115);
-	plants[FRACTAL_PLANT].maxHeight = 0.0f;
-	createPlant("gram5.txt", plants[FRACTAL_PLANT], FRACTAL_PLANT);
-
-	// create the selector cube node & attach its mesh
-	oldSelection = selection = FRACTAL_PLANT;
-	selectVelocity = 10.0f;
-	isSelectMoving = false;
-
 	// setup RTT
-	Ogre::TexturePtr rttTexture =
+	rttTexture =
 		Ogre::TextureManager::getSingleton().createManual(
 		"RttTex",
 		Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
@@ -150,7 +142,7 @@ void Application::createScene(void)
 		Ogre::PF_R8G8B8,
 		Ogre::TU_RENDERTARGET);
 
-	Ogre::RenderTexture* renderTexture = rttTexture->getBuffer()->getRenderTarget();
+	renderTexture = rttTexture->getBuffer()->getRenderTarget();
 	renderTexture->addViewport(mCamera);
 	renderTexture->getViewport(0)->setClearEveryFrame(true);
 	renderTexture->getViewport(0)->setBackgroundColour(Ogre::ColourValue::Black);
@@ -170,25 +162,81 @@ void Application::createScene(void)
 	Ogre::ColourValue fadeColour(0.0f, 0.0f, 0.0f);
 	mWindow->getViewport(0)->setBackgroundColour(fadeColour);
 
-	renderTexture->update();
-	renderTexture->writeContentsToFile("start.png");
+	/*renderTexture->update();
+	renderTexture->writeContentsToFile("start.png");*/
+
+	// create the plants
+	plants[FRACTAL_PLANT].numGenerations = 3;
+	plants[FRACTAL_PLANT].plantLocation = Ogre::Vector3(1683, 49.7f, 2115);
+	plants[FRACTAL_PLANT].maxHeight = 0.0f;
+	createPlant("gram5.txt", plants[FRACTAL_PLANT], FRACTAL_PLANT);
+
+	// create the selector cube node & attach its mesh
+	oldSelection = selection = FRACTAL_PLANT;
+	selectVelocity = 10.0f;
+	isSelectMoving = false;
+
+	// Create the quad where we render our plant
+	plantQuad = new Ogre::Rectangle2D(true);
+	plantQuad->setCorners(.5, 1.0, 1.0, .5);
+	plantQuad->setBoundingBox(Ogre::AxisAlignedBox::BOX_INFINITE);
+	Ogre::SceneNode* miniScreenNode = mSceneMgr->getRootSceneNode()->createChildSceneNode();
+	miniScreenNode->attachObject(plantQuad);
+
+	renderTexture->addListener(this);
 }
 //-------------------------------------------------------------------------------------
 void Application::createFrameListener(void)
 {
 	BaseApplication::createFrameListener();
 
+	// Setup GUI for the demo
 	mInfoLabel = mTrayMgr->createLabel(OgreBites::TL_TOP, "TInfo", "", 350);
 	mTrayMgr->removeWidgetFromTray(mInfoLabel);
 	mInfoLabel->hide();
+	mTrayMgr->hideLogo();
 
-	mDiffuseColorRSlider = mTrayMgr->createLongSlider(OgreBites::TL_LEFT, "TSDiffuseR", "Diffuse Color R", 50,
+	// GUI for setting diffuse properties of tree material
+	mTrayMgr->setTrayWidgetAlignment(OgreBites::TL_RIGHT, Ogre::GuiHorizontalAlignment::GHA_CENTER);
+	mTrayMgr->createLabel(OgreBites::TL_RIGHT, "TLDiffuse", "Diffuse", 100.0f);
+	mDiffuseColorRSlider = mTrayMgr->createLongSlider(OgreBites::TL_RIGHT, "TSDiffuseR", "R", 50,
 		60, 0, 255, 256);
-	mDiffuseColorGSlider = mTrayMgr->createLongSlider(OgreBites::TL_LEFT, "TSDiffuseG", "Diffuse Color G", 50,
+	mDiffuseColorGSlider = mTrayMgr->createLongSlider(OgreBites::TL_RIGHT, "TSDiffuseG", "G", 50,
 		60, 0, 255, 256);
-	mDiffuseColorBSlider = mTrayMgr->createLongSlider(OgreBites::TL_LEFT, "TSDiffuseB", "Diffuse Color B", 50,
+	mDiffuseColorBSlider = mTrayMgr->createLongSlider(OgreBites::TL_RIGHT, "TSDiffuseB", "B", 50,
 		60, 0, 255, 256);
 
+	mTrayMgr->createSeparator(OgreBites::TL_RIGHT, "TSeparator1", 150.0f);
+
+	// GUI for setting specular properties of tree material
+	mTrayMgr->createLabel(OgreBites::TL_RIGHT, "TLSpecular", "Specular", 100.0f);
+	mSpecularColorRSlider = mTrayMgr->createLongSlider(OgreBites::TL_RIGHT, "TSSpecularR", "R", 50,
+		60, 0, 255, 256);
+	mSpecularColorGSlider = mTrayMgr->createLongSlider(OgreBites::TL_RIGHT, "TSSpecularG", "G", 50,
+		60, 0, 255, 256);
+	mSpecularColorBSlider = mTrayMgr->createLongSlider(OgreBites::TL_RIGHT, "TSSpecularB", "B", 50,
+		60, 0, 255, 256);
+
+	mTrayMgr->createSeparator(OgreBites::TL_RIGHT, "TSeparator2", 150.0f);
+
+	// GUI for setting angle properties
+	mTrayMgr->createLabel(OgreBites::TL_RIGHT, "TLAngle", "Angle", 100.0f);
+	mAngleSlider = mTrayMgr->createLongSlider(OgreBites::TL_RIGHT, "TSAngle", "", 50,
+		60, 0, 90, 91);
+	mAngleSlider->setValue(Ogre::Math::Floor(angle * 180.0f / Ogre::Math::PI), true);
+
+	mTrayMgr->createSeparator(OgreBites::TL_RIGHT, "TSeparator3", 150.0f);
+
+	// GUI for setting generations
+	mTrayMgr->createLabel(OgreBites::TL_RIGHT, "TLNumGen", "Generations", 150.0f);
+	mNumGenerationsSlider = mTrayMgr->createLongSlider(OgreBites::TL_RIGHT, "TSNumGen", "", 50,
+		60, 0, 6, 7);
+
+	mTrayMgr->createSeparator(OgreBites::TL_RIGHT, "TSeparator4", 150.0f);
+
+	// Buttons for Generating the trees and running the demo
+	mGenerateButton = mTrayMgr->createButton(OgreBites::TL_RIGHT, "TGenButton", "Generate", 100);
+	mRunDemoButton = mTrayMgr->createButton(OgreBites::TL_RIGHT, "TRunButton", "Run Demo", 100);;
 
 }
 //-------------------------------------------------------------------------------------
@@ -403,16 +451,12 @@ bool Application::keyPressed(const OIS::KeyEvent &arg)
 	else if (arg.key == OIS::KC_ADD)
 	{
 		plants[selection].numGenerations++;
-		mSceneMgr->destroySceneNode(plants[FRACTAL_PLANT].plantNode);
-		plants[FRACTAL_PLANT].maxHeight = 0.0f;
-		createPlant("gram5.txt", plants[FRACTAL_PLANT], FRACTAL_PLANT);
+		resetPlant();
 	}
 	else if (arg.key == OIS::KC_SUBTRACT)
 	{
 		if (plants[selection].numGenerations > 0) plants[selection].numGenerations--;
-		mSceneMgr->destroySceneNode(plants[FRACTAL_PLANT].plantNode);
-		plants[FRACTAL_PLANT].maxHeight = 0.0f;
-		createPlant("gram5.txt", plants[FRACTAL_PLANT], FRACTAL_PLANT);
+		resetPlant();
 	}
 	else if (arg.key == OIS::KC_LEFT)
 	{
@@ -451,7 +495,40 @@ bool Application::keyReleased(const OIS::KeyEvent &arg)
 // Sdk Tray listeners
 void Application::sliderMoved(OgreBites::Slider* slider)
 {
-	//slider->setValue(1.0f, false);
+	if (slider == mDiffuseColorRSlider)
+		diffuseRed = slider->getValue() / 255.0f;
+	else if (slider == mDiffuseColorGSlider)
+		diffuseGreen = slider->getValue() / 255.0f;
+	else if (slider == mDiffuseColorBSlider)
+		diffuseBlue = slider->getValue() / 255.0f;
+	else if (slider == mSpecularColorRSlider) 
+		specularRed = slider->getValue() / 255.0f;
+	else if (slider == mSpecularColorGSlider)
+		specularGreen = slider->getValue() / 255.0f;
+	else if (slider == mSpecularColorBSlider)
+		specularBlue = slider->getValue() / 255.0f;
+	else if (slider == mAngleSlider)
+		angle = mAngleSlider->getValue() * Ogre::Math::PI / 180.0f;
+	else if (slider == mNumGenerationsSlider)
+		plants[FRACTAL_PLANT].numGenerations = mNumGenerationsSlider->getValue();
+}
+void Application::buttonHit(OgreBites::Button* bt)
+{
+	if (bt == mGenerateButton)
+	{
+		resetPlant();
+	}
+	else if (bt == mRunDemoButton)
+	{
+
+	}
+}
+
+void Application::resetPlant()
+{
+	//mSceneMgr->destroySceneNode(plants[FRACTAL_PLANT].plantNode);
+	plants[FRACTAL_PLANT].maxHeight = 0.0f;
+	createPlant("gram5.txt", plants[FRACTAL_PLANT], FRACTAL_PLANT);
 }
 
 void Application::createPlant(const std::string& filename, Plant_t& plant, SystemType type)
@@ -482,30 +559,33 @@ void Application::createPlant(const std::string& filename, Plant_t& plant, Syste
 		IdentifierNode* idNode = new IdentifierNode("F");
 		//IdentifierNode* idNode2;
 		std::vector<Node*> symbols;
-		std::string name;
-		Ogre::Real angle;
+
 		switch (type)
 		{
 		case FRACTAL_PLANT:
 			idNode = new IdentifierNode("X");
 			symbols.push_back(idNode);
-			name = "FRACTAL PLANT";
-			angle = Ogre::Math::PI / 6;
 			break;
 		case PYTHAGORAS_TREE:
 			idNode = new IdentifierNode("A");
 			symbols.push_back(idNode);
-			name = "PYTHAGORAS TREE";
-			angle = Ogre::Math::PI / 4;
 			break;
 		default:
 			idNode = 0;
 			break;
 		}
 
+		// create a material for our plant
+		Ogre::MaterialPtr mat = Ogre::MaterialManager::getSingleton().create(Ogre::String("mat").c_str(),
+			Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
+
+		mat->setAmbient(1.0f, 1.0f, 1.0f);
+		mat->setDiffuse(diffuseRed, diffuseGreen, diffuseBlue, 1.0f);
+		mat->setSpecular(specularRed, specularGreen, specularBlue, 1.0f);
+
 		// Renderer visitor - will create our entities according to the grammar nodes it visits
 		plant.plantNode = mSceneMgr->getRootSceneNode()->createChildSceneNode();
-		RendererVisitor rendererVis(mSceneMgr, plant.plantNode, plant.plantLocation, name, angle);
+		RendererVisitor rendererVis(mSceneMgr, plant.plantNode, plant.plantLocation, angle, mat);
 
 		// Evolve the generations
 		for (Ogre::int32 i = 0; i < plant.numGenerations; i++)
@@ -522,10 +602,104 @@ void Application::createPlant(const std::string& filename, Plant_t& plant, Syste
 
 		Ogre::Real h = abs(rendererVis.getMaxHeightReached() - 49.7f);
 		mCamera->setPosition(Ogre::Vector3(1683.0f, 49.7f + h / 2, 2115.0f + h / atan(1.0f)));
+
+		plantChanged = true;
+		renderToTexture();
 	}
 	else
 	{
 		std::cout << "Error: unable to open file: " << filename << std::endl;
 		plant.plantNode = 0;
 	}
+}
+
+void Application::go(void)
+{
+#ifdef _DEBUG
+#ifndef OGRE_STATIC_LIB
+	mResourcesCfg = m_ResourcePath + "resources_d.cfg";
+	mPluginsCfg = m_ResourcePath + "plugins_d.cfg";
+#else
+	mResourcesCfg = "resources_d.cfg";
+	mPluginsCfg = "plugins_d.cfg";
+#endif
+#else
+#ifndef OGRE_STATIC_LIB
+	mResourcesCfg = m_ResourcePath + "resources.cfg";
+	mPluginsCfg = m_ResourcePath + "plugins.cfg";
+#else
+	mResourcesCfg = "resources.cfg";
+	mPluginsCfg = "plugins.cfg";
+#endif
+#endif
+
+	if (!setup())
+		return;
+
+	//while (!mWindow->isClosed() && !mShutDown);
+	//mRoot->startRendering();
+
+	//assert(mActiveRenderer != 0);
+
+	//mActiveRenderer->_initRenderTargets();
+
+	// Clear event times
+	mRoot->clearEventTimes();
+
+	// Infinite loop, until broken out of by frame listeners
+	// or break out by calling queueEndRendering()
+	//mQueuedEnd = false;
+
+	mSceneMgr->addRenderQueueListener(this);
+	mRoot->renderOneFrame();
+
+	while (!mWindow->isClosed() && !mShutDown)
+	{
+		//Pump messages in all registered RenderWindow windows
+		Ogre::WindowEventUtilities::messagePump();
+
+		/*if (plantChanged)
+		{
+			mRoot->renderOneFrame();
+			plantChanged = false;
+		}*/
+		bool ret = mRoot->renderOneFrame();
+		if (plantChanged)
+		{
+			mSceneMgr->destroySceneNode(plants[FRACTAL_PLANT].plantNode);
+			plantChanged = false;
+		}
+		if (!ret)
+			break;
+	}
+
+	// Clean up
+	destroyScene();
+}
+
+void Application::renderQueueEnded(Ogre::uint8 queueGroupId, const Ogre::String& invocation, bool& repeatThisInvocation)
+{
+
+}
+
+void Application::renderQueueStarted(Ogre::uint8 queueGroupId, const Ogre::String& invocation, bool& skipThisInvocation)
+{
+	/*if (queueGroupId == 50)
+		skipThisInvocation = true;*/
+}
+
+void Application::renderToTexture(const Ogre::String& filename)
+{
+	renderTexture->update();
+	renderTexture->writeContentsToFile(filename);
+}
+
+void Application::preRenderTargetUpdate(const Ogre::RenderTargetEvent& rte)
+{
+	plantQuad->setVisible(false);
+}
+
+void Application::postRenderTargetUpdate(const Ogre::RenderTargetEvent& rte)
+{
+	plantQuad->setVisible(true);
 }
--- ASiameseDream/Application.h
+++ ASiameseDream/Application.h
@@ -10,19 +10,26 @@
 
 class Submarine;
 
-class Application : public BaseApplication
+class Application : public BaseApplication, Ogre::RenderQueueListener, public Ogre::RenderTargetListener
 {
 private:
 	Ogre::TerrainGlobalOptions* mTerrainGlobals;
 	Ogre::TerrainGroup* mTerrainGroup;
 	OgreBites::Label* mInfoLabel;
-	OgreBites::Slider* mDiffuseColorRSlider;
-	OgreBites::Slider* mDiffuseColorGSlider;
-	OgreBites::Slider* mDiffuseColorBSlider;
 	bool mTerrainsImported;
 
 	// Keep our objects here
 	Submarine*		sub;
+	OgreBites::Slider* mDiffuseColorRSlider;
+	OgreBites::Slider* mDiffuseColorGSlider;
+	OgreBites::Slider* mDiffuseColorBSlider;
+	OgreBites::Slider* mSpecularColorRSlider;
+	OgreBites::Slider* mSpecularColorGSlider;
+	OgreBites::Slider* mSpecularColorBSlider;
+	OgreBites::Slider* mAngleSlider;
+	OgreBites::Slider* mNumGenerationsSlider;
+	OgreBites::Button* mGenerateButton;
+	OgreBites::Button* mRunDemoButton;
 	Ogre::Vector2	cameraVelocity; // TODO: remove this when project is integrated
 	Ogre::Real		selectVelocity; // TODO: remove this when project is integrated
 	Ogre::int32		selection;		// TODO: remove this when project is integrated
@@ -36,6 +43,14 @@ private:
 		Ogre::int32		 numGenerations;
 		Ogre::Real		 maxHeight;
 	} plants[MAX_PLANTS];
+	Ogre::Real	diffuseRed, diffuseGreen, diffuseBlue;
+	Ogre::Real	specularRed, specularGreen, specularBlue;
+	Ogre::Real  angle;
+	const Ogre::Real INIT_ANGLE;
+	bool plantChanged;
+	Ogre::Rectangle2D* plantQuad;
+	Ogre::RenderTexture* renderTexture;
+	Ogre::TexturePtr rttTexture;
 
 	void defineTerrain(long x, long y);
 	void initBlendMaps(Ogre::Terrain* terrain);
@@ -44,8 +59,19 @@ public:
 	Application(void);
 	virtual ~Application(void);
 
+	virtual void go(void);
+
 	// Sdk Tray listeners
 	virtual void sliderMoved(OgreBites::Slider* slider);
+	virtual void buttonHit(OgreBites::Button* bt);
+
+	virtual void renderQueueEnded(Ogre::uint8 queueGroupId, const Ogre::String& invocation,
+		bool& repeatThisInvocation);
+	virtual void renderQueueStarted(Ogre::uint8 queueGroupId, const Ogre::String& invocation,
+		bool& skipThisInvocation);
+
+	virtual void preRenderTargetUpdate(const Ogre::RenderTargetEvent& rte);
+	virtual void postRenderTargetUpdate(const Ogre::RenderTargetEvent& rte);
 protected:
 	virtual void createScene(void);
 	virtual void createFrameListener(void);
@@ -53,6 +79,8 @@ protected:
 	virtual bool frameRenderingQueued(const Ogre::FrameEvent& evt);
 
 	void createPlant(const std::string& filename, Plant_t& plant, SystemType type);
+	void resetPlant();
+	void renderToTexture(const Ogre::String& filename = "treeTexture.png");
 
 	// IO Listeners
 	virtual bool mouseMoved(const OIS::MouseEvent& evt);
